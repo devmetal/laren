1ffdc4bf9ae83396f1087e5c64aa3ee357ffc57c
Metál Ádám <devmetal91@gmail.com>
2016-09-27 13:46:41 +0200

    add eslint to ci
    Plus build eval.js

--- es2015/eval.js
+++ es2015/eval.js
@@ -1,19 +1,17 @@
 'use strict';
 
-var log = require('simple-node-logger').createSimpleLogger();
-
 module.exports = function createUserFunc(code) {
   try {
     var fn = eval(code); // eslint-disable-line no-eval
     if (!(typeof fn === 'function')) {
-      log.error('No function given');
+      console.log('No function given');
       return false;
     }
     return fn;
   } catch (e) {
-    log.error('Error in your function! Check syntax');
-    log.error(e.message);
-    log.error(e.stack);
+    console.log('Error in your function! Check syntax');
+    console.log(e.message);
+    console.log(e.stack);
     return false;
   }
 };
